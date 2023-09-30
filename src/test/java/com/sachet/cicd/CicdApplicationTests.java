74976d52bf5200a183c65928edd5d09750e02326
sachetwasti1997 <sachetwasti61@gmail.com>
2023-09-30 13:39:05 +0530

    first commit

---
+++ src/test/java/com/sachet/cicd/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachet.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
